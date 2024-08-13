90558cb44522d6804d09ffaa7e2140cdad85aac4
UlviParviz <ulvispaethe@gmail.com>
2024-08-13 16:05:17 +0100

    added dto and get restaurant by id functionality

--- src/restaurants/restaurants.controller.ts
+++ src/restaurants/restaurants.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { RestaurantsService } from './restaurants.service';
 import { Restaurant } from './schemas/restaurant.schema';
+import { CreateRestaurantDto } from './dto/create-restaurant.dto';
 
 @Controller('restaurants')
 export class RestaurantsController {
@@ -14,8 +15,16 @@ export class RestaurantsController {
   @Post()
   async createRestaurant(
     @Body()
-    restaurant,
+    restaurant: CreateRestaurantDto,
   ): Promise<Restaurant[]> {
     return this.restaurantService.create(restaurant);
+  }
+
+  @Get(':id')
+  async getRestaurant(
+    @Param('id')
+    id: string,
+  ): Promise<Restaurant[]> {
+    return this.restaurantService.findById(id);
   }
 }
