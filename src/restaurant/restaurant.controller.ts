1c62d3511d8c9cfacd88a306691ee69c4bf26785
UlviParviz <ulvispaethe@gmail.com>
2024-08-14 09:25:35 +0100

    added search by name functionality

--- src/restaurant/restaurant.controller.ts
+++ src/restaurant/restaurant.controller.ts
@@ -6,7 +6,9 @@ import {
   Param,
   Post,
   Put,
+  Query,
 } from '@nestjs/common';
+import { Query as ExpressQuery } from 'express-serve-static-core';
 import { RestaurantsService } from './restaurant.service';
 import { Restaurant } from './schemas/restaurant.schema';
 import { CreateRestaurantDto } from './dto/create-restaurant.dto';
@@ -17,8 +19,8 @@ export class RestaurantsController {
   constructor(private restaurantService: RestaurantsService) {}
 
   @Get()
-  async getAllRestaurants(): Promise<Restaurant[]> {
-    return this.restaurantService.findAll();
+  async getAllRestaurants(@Query() query: ExpressQuery): Promise<Restaurant[]> {
+    return this.restaurantService.findAll(query);
   }
 
   @Post()
