265eab4fa530508244533b694981489f87128a5b
UlviParviz <ulvispaethe@gmail.com>
2024-08-22 20:16:29 +0100

    handled duplicate email error

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
@@ -18,11 +18,20 @@ export class AuthService {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = await this.userModel.create({
-      name,
-      email,
-      password: hashedPassword,
-    });
-    return user;
+    try {
+      const user = await this.userModel.create({
+        name,
+        email,
+        password: hashedPassword,
+      });
+
+      return user;
+    } catch (error) {
+      //Handle duplicate email
+      if (error.code == 11000) {
+        throw new ConflictException('Duplicate email entered');
+      }
+      
+    }
   }
 }
