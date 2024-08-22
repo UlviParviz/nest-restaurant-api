16c631945e743f982b1ca96d29d41fdb56ab17db
UlviParviz <ulvispaethe@gmail.com>
2024-08-22 21:10:24 +0100

    added login functionality

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,14 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
 import { SignUpDto } from './dto/signup.dto';
 import * as bcrypt from 'bcryptjs';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -31,7 +36,24 @@ export class AuthService {
       if (error.code == 11000) {
         throw new ConflictException('Duplicate email entered');
       }
-      
     }
+  }
+
+  async login(loginDto: LoginDto): Promise<User> {
+    const { email, password } = loginDto;
+
+    const user = await this.userModel.findOne({ email }).select('+password');
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid email address or password');
+    }
+
+    const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatched) {
+      throw new UnauthorizedException('Invalid email address or password');
+    }
+
+    return user;
   }
 }
