9b1abfa22eb332c3b4fd7e060f32a60d480af119
UlviParviz <ulvispaethe@gmail.com>
2024-09-02 15:23:49 +0100

    added register service test

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,9 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { getModelToken } from '@nestjs/mongoose';
-import { User } from './schemas/user.schema';
+import { User, UserRoles } from './schemas/user.schema';
 import { Model } from 'mongoose';
 import { JwtModule } from '@nestjs/jwt';
+import * as bcrypt from 'bcryptjs';
+import APIFeatures from '../utils/features.utils';
+import { ConflictException } from '@nestjs/common';
+
+const mockUser = {
+  _id: '61c0ccf11d7bf83d153d7c06',
+  email: 'ulvi666@gmail.com',
+  name: 'Ulvi',
+  role: UserRoles.USER,
+  password: 'hashedPassword',
+};
+
+const token = 'jwtToken';
 
 const mockAuthService = {
   create: jest.fn(),
@@ -14,14 +27,14 @@ describe('AuthService', () => {
   let service: AuthService;
   let model: Model<User>;
 
-  beforeEach( async () => {
+  beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-        imports: [
-            JwtModule.register({
-                secret: 'KNAKSDJAKSLDJALKSDJALKSD',
-                signOptions: { expiresIn: '1d' },
-            })
-        ],
+      imports: [
+        JwtModule.register({
+          secret: 'KNAKSDJAKSLDJALKSDJALKSD',
+          signOptions: { expiresIn: '1d' },
+        }),
+      ],
       providers: [
         AuthService,
         {
@@ -37,5 +50,35 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('signUp', () => {
+    const signUpDto = {
+      name: 'Ulvi',
+      email: 'ulvi666@gmail.com',
+      password: '12345678',
+    };
+
+    it('should register a new user', async () => {
+      jest.spyOn(bcrypt, 'hash').mockResolvedValueOnce('testHash');
+      jest
+        .spyOn(model, 'create')
+        .mockImplementationOnce(() => Promise.resolve(mockUser as any));
+
+      jest.spyOn(APIFeatures, 'assignJwtToken').mockResolvedValueOnce(token);
+
+      const result = await service.signUp(signUpDto);
+
+      expect(bcrypt.hash).toHaveBeenCalled();
+      expect(result.token).toEqual(token);
+    });
+
+    it('should throw duplicate email entered', async () => {
+      jest
+        .spyOn(model, 'create')
+        .mockImplementationOnce(() => Promise.reject({ code: 11000 }));
+
+      expect(service.signUp(signUpDto)).rejects.toThrow(ConflictException);
+    });
   });
 });
