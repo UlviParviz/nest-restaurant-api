8e2885616cd555b054b57ff9dda0b0b303eba59a
UlviParviz <ulvispaethe@gmail.com>
2024-09-02 07:44:30 +0100

    added get all restaurants controller test

--- src/restaurant/restaurant.controller.spec.ts
+++ src/restaurant/restaurant.controller.spec.ts
@@ -1,8 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RestaurantsController } from './restaurant.controller';
 import { RestaurantsService } from './restaurant.service';
+import { UserRoles } from '../auth/schemas/user.schema';
 
-const mockRestaurantService = {};
+const mockRestaurant = {
+  user: '61c0ccf11d7bf83d153d7c06',
+  menu: [],
+  location: {
+    type: 'Point',
+    coordinates: [-77.376204, 38.492151],
+    formattedAddress: '200 Olympic Dr, Stafford, VA 22554-7763, US',
+    city: 'Stafford',
+    state: 'VA',
+    zipcode: '22554-7763',
+    country: 'US',
+  },
+  images: [],
+  category: 'Fast Food',
+  address: '200 Olympic Dr, Stafford, VS, 22554',
+  phoneNo: 9788246116,
+  email: 'ghulam@gamil.com',
+  description: 'This is just a description',
+  name: 'Retaurant 4',
+  _id: '61c4aa2ffaa767823d1687ef',
+  createdAt: '2021-12-23T16:56:15.127Z',
+  updatedAt: '2021-12-23T16:56:15.127Z',
+};
+
+const mockUser = {
+  _id: '61c0ccf11d7bf83d153d7c06',
+  email: 'ghulam1@gmail.com',
+  name: 'Ghulam',
+  role: UserRoles.USER,
+};
+
+const mockRestaurantService = {
+  findAll: jest.fn().mockResolvedValueOnce([mockRestaurant]),
+};
 describe('RestaurantController', () => {
   let controller: RestaurantsController;
   let service: RestaurantsService;
@@ -24,5 +58,16 @@ describe('RestaurantController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
-  })
+  });
+
+  describe('getAllRestaurants', () => {
+    it('should get all restaurants', async () => {
+      const result = await controller.getAllRestaurants({
+        keyword: 'restaurant',
+      });
+
+      expect(service.findAll).toHaveBeenCalled();
+      expect(result).toEqual([mockRestaurant]);
+    });
+  });
 });
