682cf26552b523a51b926a652bbf732d09f88cff
UlviParviz <ulvispaethe@gmail.com>
2024-09-03 15:51:37 +0100

    added env files

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
-  // app.setGlobalPrefix('api')
+  // Optional: Set a global prefix
+  // app.setGlobalPrefix('api');
   
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 3000);
+  await app.init();
 }
+
 bootstrap();
+
+export default server;
